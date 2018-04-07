f3256fb68ec281979177e3038773b39f1f95cf26
James Childers <jamesalexchilders@gmail.com>
2018-04-06 22:13:57 -0500

    Created a string scrambler to help teach Kevin.

---
+++ StringScrambler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringScrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringScrambler scrambler = new StringScrambler();
+            scrambler.run();
+        }
+    }
+}
---
+++ StringScrambler/StringScrambler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringScrambler
+{
+    public class StringScrambler
+    {
+        private Random random = new Random();
+
+        public void run()
+        {
+            HandleInput();
+        }
+
+        public void HandleInput()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter your first sentence");
+            string input = Console.ReadLine();
+            Console.WriteLine("Please enter your second sentence");
+            string input2 = Console.ReadLine();
+            var scrambledSentence = scrambleTogether(input, input2);
+            Console.WriteLine("Your scrambled string is: ");
+            Console.WriteLine(scrambledSentence);
+            Console.WriteLine("Enter 1 to play again, enter anything else to exit");
+            string input3 = Console.ReadLine();
+            if(input3 != "1")
+            {
+                Exit();
+            }
+            else
+            {
+                HandleInput();
+            }
+        }
+
+        private string scrambleTogether(string sentence1, string sentence2)
+        {
+           string scrambledString = "";
+           List<string> words = sentence1.Split(' ').ToList();
+           List<string> split2 = sentence2.Split(' ').ToList() ;
+           words.AddRange(split2);
+            int totalWords = words.Count;
+
+           for(var i = 0; i < totalWords; i++)
+            {
+                string word = pickRandomWord(words);
+                words.Remove(word);
+                scrambledString += " " + word;
+            }
+            return scrambledString;
+        }
+
+        private string pickRandomWord(List<string> words)
+        {
+            return words[random.Next(words.Count - 1)];
+        }
+
+        public void Exit()
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
